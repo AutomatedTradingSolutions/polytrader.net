d5a236146933bc1d6b546bcb61575fc4a8d17517
c-snowden <csnowden@automatedtradingsolutions.co.uk>
2017-10-20 15:46:53 +0100

    First draft DB methods python script

---
+++ common/python3/db_methods.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# start with the python debugger. should be commented before release.
+import pdb
+
+import sys
+
+import logging
+import typing
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('sqla.engine.base').setLevel(logging.DEBUG)
+
+from data_object import DataObject
+
+class GetDatabaseInstance:
+
+  def __init__(self, db_):
+    self._db = db_
+
+  def Select(self, class_, procName, filter_, recordsExpected = None):
+    connection_ = self._db.GetConnection('ro')
+    result_ = self._db.CallProcedure(connection_, procName, filter_)
+    if recordsExpected != None and len(result_) != recordsExpected:
+      raise ValueError('Got {actual} record(s) but expected {expected}'.format(actual=len(result_), expected=recordsExpected))
+    return [ class_(row_) for row_ in result_ ]
+
+  def Insert():
+    pass
