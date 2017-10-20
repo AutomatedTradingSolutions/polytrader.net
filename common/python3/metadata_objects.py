b33e33e3c9be1d789ed512051d8ace4ff2477e8d
c-snowden <csnowden@automatedtradingsolutions.co.uk>
2017-10-20 14:35:40 +0100

    Added frequency class

--- common/python3/metadata_objects.py
+++ common/python3/metadata_objects.py
@@ -13,6 +13,34 @@ logging.getLogger('sqla.engine.base').setLevel(logging.DEBUG)
 
 from data_object import DataObject
 
+class AssetClass(DataObject):
+
+  @staticmethod
+  def GetterProcedureName():
+    return 'GetAssetClass'
+
+  @classmethod
+  def GetDatabaseInstance(class_, db_, filter_):
+    connection_ = db_.GetConnection('ro')
+    result_ = db_.CallProcedure(connection_, __class__.GetterProcedureName(), filter_)
+    if len(result_) != 1:
+      raise ValueError('{instance} not found error'.format(instance=filter_))
+    row_ = result_[0]
+    return class_(row_)
+
+  def GetId(self):
+    return self._id
+
+  def GetCode(self):
+    return self._code
+
+  def GetName(self):
+    return self._name
+
+AssetClass.AddClassMember('_id')
+AssetClass.AddClassMember('_code', sourceName = 'ASSCLSCODE', pkFlag = True)
+AssetClass.AddClassMember('_name')
+
 class Frequency(DataObject):
 
   @staticmethod
